3087a8e8c995b7cc0bdf9de6fa48651371c9d88b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2022-03-09 15:36:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,14 @@ module.exports = {
     'no-multi-spaces': [
       'error'
     ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        'max': 2,
+        'maxBOF': 0,
+        'maxEOF': 0
+      }
+    ],
     // Remove unused defined variables
     'no-unused-vars': [
       'error'
